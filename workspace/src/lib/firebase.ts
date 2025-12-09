670a8144852720f23bc3753301c24c7004ef2ac8
Thariq <attendrys@gmail.com>
2025-12-09 05:16:57 +0000

    update on env

--- workspace/src/lib/firebase.ts
+++ workspace/src/lib/firebase.ts
@@ -11,13 +11,13 @@ import { getAnalytics, type Analytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA7HLhCmNQ1Pqq0hdfz28fv1uD7EgmhnZY",
-  authDomain: "attendry-a6f9b.firebaseapp.com",
-  projectId: "attendry-a6f9b",
-  storageBucket: "attendry-a6f9b.appspot.com",
-  messagingSenderId: "166352358207",
-  appId: "1:166352358207:web:c663ad6c38352dc831218a",
-  measurementId: "G-6GEVRXQL9M"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
